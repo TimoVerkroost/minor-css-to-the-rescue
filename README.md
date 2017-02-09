0b941a3e57a5b7e79937b8be84bcc68d15815758
Timo Verkroost <timo.verkroost@gmail.com>
2017-02-09 14:35:39 +0100

    comps delete wrap div

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # minor-css-to-the-rescue
 All assignments for CSS To The Rescue
+
+## Assignments
+- Styleguide: https://timoverkroost.github.io/minor-css-to-the-rescue/srv/pattern-library/styleguide.html
--- srv/pattern-library/js/w3data.js
+++ srv/pattern-library/js/w3data.js
@@ -1,7 +1,8 @@
-/* W3Data ver 1.31 by W3Schools.com */
 var w3DataObject = {};
+
 function w3DisplayData(id, data) {
-    var htmlObj, htmlTemplate, html, arr = [], a, l, rowClone, x, j, i, ii, cc, repeat, repeatObj, repeatX = "";
+    var htmlObj, htmlTemplate, html, arr = [],
+        a, l, rowClone, x, j, i, ii, cc, repeat, repeatObj, repeatX = "";
     htmlObj = document.getElementById(id);
     htmlTemplate = w3InitTemplate(id, htmlObj);
     html = htmlTemplate.cloneNode(true);
@@ -27,7 +28,7 @@ function w3DisplayData(id, data) {
                 for (ii = 0; ii < a.length; ii += 1) {
                     a[ii].value = w3NeedleInHaystack(a[ii], "attribute", repeatX, repeatObj[x]).value;
                 }
-                (i === repeatObj.length) ? arr[j].parentNode.replaceChild(rowClone, arr[j]) : arr[j].parentNode.insertBefore(rowClone, arr[j]);
+                (i === repeatObj.length) ? arr[j].parentNode.replaceChild(rowClone, arr[j]): arr[j].parentNode.insertBefore(rowClone, arr[j]);
             }
         } else {
             console.log("w3-repeat must be an array. " + repeat + " is not an array.");
@@ -36,30 +37,46 @@ function w3DisplayData(id, data) {
     }
     html = w3NeedleInHaystack(html, "element");
     htmlObj.parentNode.replaceChild(html, htmlObj);
+
     function w3InitTemplate(id, obj) {
         var template;
         template = obj.cloneNode(true);
-        if (w3DataObject.hasOwnProperty(id)) {return w3DataObject[id];}
+        if (w3DataObject.hasOwnProperty(id)) {
+            return w3DataObject[id];
+        }
         w3DataObject[id] = template;
         return template;
     }
+
     function w3GetElementsByAttribute(x, att) {
-        var arr = [], arrCount = -1, i, l, y = x.getElementsByTagName("*"), z = att.toUpperCase();
+        var arr = [],
+            arrCount = -1,
+            i, l, y = x.getElementsByTagName("*"),
+            z = att.toUpperCase();
         l = y.length;
         for (i = -1; i < l; i += 1) {
-            if (i == -1) {y[i] = x;}
-            if (y[i].getAttribute(z) !== null) {arrCount += 1; arr[arrCount] = y[i];}
+            if (i == -1) {
+                y[i] = x;
+            }
+            if (y[i].getAttribute(z) !== null) {
+                arrCount += 1;
+                arr[arrCount] = y[i];
+            }
         }
         return arr;
     }
+
     function w3NeedleInHaystack(elmnt, typ, repeatX, x) {
-        var value, rowClone, pos1, haystack, pos2, needle = [], needleToReplace, i, cc, r;
+        var value, rowClone, pos1, haystack, pos2, needle = [],
+            needleToReplace, i, cc, r;
         rowClone = elmnt.cloneNode(true);
         pos1 = 0;
         while (pos1 > -1) {
             haystack = (typ == "attribute") ? rowClone.value : rowClone.innerHTML;
             pos1 = haystack.indexOf("{{", pos1);
-            if (pos1 === -1) {break;}
+            if (pos1 === -1) {
+                break;
+            }
             pos2 = haystack.indexOf("}}", pos1 + 1);
             needleToReplace = haystack.substring(pos1 + 2, pos2);
             needle = needleToReplace.split("||");
@@ -67,23 +84,33 @@ function w3DisplayData(id, data) {
             for (i = 0; i < needle.length; i += 1) {
                 needle[i] = needle[i].replace(/^\s+|\s+$/gm, ''); //trim
                 //value = ((x && x[needle[i]]) || (data && data[needle[i]]));
-                if (x) {value = x[needle[i]];}
-                if (value == undefined && data) {value = data[needle[i]];}
-                if (value == undefined) {
-                    cc = needle[i].split(".");
-                    if (cc[0] == repeatX) {value = x[cc[1]]; }
+                if (x) {
+                    value = x[needle[i]];
+                }
+                if (value == undefined && data) {
+                    value = data[needle[i]];
                 }
                 if (value == undefined) {
-                    if (needle[i] == repeatX) {value = x;}
+                    cc = needle[i].split(".");
+                    if (cc[0] == repeatX) {
+                        value = x[cc[1]];
+                    }
+                }
+                if (value == undefined) {
+                    if (needle[i] == repeatX) {
+                        value = x;
+                    }
                 }
                 if (value == undefined) {
                     if (needle[i].substr(0, 1) == '"') {
                         value = needle[i].replace(/"/g, "");
-                    } else if (needle[i].substr(0,1) == "'") {
+                    } else if (needle[i].substr(0, 1) == "'") {
                         value = needle[i].replace(/'/g, "");
                     }
                 }
-                if (value != undefined) {break;}
+                if (value != undefined) {
+                    break;
+                }
             }
             if (value != undefined) {
                 r = "{{" + needleToReplace + "}}";
@@ -97,13 +124,16 @@ function w3DisplayData(id, data) {
         }
         return rowClone;
     }
+
     function w3ReplaceHTML(a, r, result) {
         var b, l, i, a, x, j;
         if (a.hasAttributes()) {
             b = a.attributes;
             l = b.length;
             for (i = 0; i < l; i += 1) {
-                if (b[i].value.indexOf(r) > -1) {b[i].value = b[i].value.replace(r, result);}
+                if (b[i].value.indexOf(r) > -1) {
+                    b[i].value = b[i].value.replace(r, result);
+                }
             }
         }
         x = a.getElementsByTagName("*");
@@ -111,37 +141,43 @@ function w3DisplayData(id, data) {
         a.innerHTML = a.innerHTML.replace(r, result);
     }
 }
+
 function w3IncludeHTML() {
-  var z, i, elmnt, file, xhttp;
-  z = document.getElementsByTagName("*");
-  for (i = 0; i < z.length; i++) {
-    elmnt = z[i];
-    file = elmnt.getAttribute("w3-include-html");
-    if (file) {
-      xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-          elmnt.innerHTML = this.responseText;
-          elmnt.removeAttribute("w3-include-html");
-          w3IncludeHTML();
+    var z, i, elmnt, file, xhttp;
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        file = elmnt.getAttribute("w3-include-html");
+        if (file) {
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    elmnt.innerHTML = this.responseText;
+                    elmnt.removeAttribute("w3-include-html");
+                    w3IncludeHTML();
+                }
+            }
+            xhttp.open("GET", file, true);
+            xhttp.send();
+            return;
         }
-      }      
-      xhttp.open("GET", file, true);
-      xhttp.send();
-      return;
     }
-  }
 }
+
 function w3Http(target, readyfunc, xml, method) {
     var httpObj;
-    if (!method) {method = "GET"; }
+    if (!method) {
+        method = "GET";
+    }
     if (window.XMLHttpRequest) {
         httpObj = new XMLHttpRequest();
     } else if (window.ActiveXObject) {
         httpObj = new ActiveXObject("Microsoft.XMLHTTP");
     }
     if (httpObj) {
-        if (readyfunc) {httpObj.onreadystatechange = readyfunc;}
+        if (readyfunc) {
+            httpObj.onreadystatechange = readyfunc;
+        }
         httpObj.open(method, target, true);
         httpObj.send(xml);
     }
